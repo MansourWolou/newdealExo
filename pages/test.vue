68c1e0fd025d3a35b730885aab7ae301abc12ae0
MohamedWOLOU <mohamed.wolou@etu.univ-nantes.fr>
2022-09-09 11:26:30 +0200

    config axios and request page done

---
+++ pages/test.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+      name:'Test',
+        async asyncData({ $axios }) {
+          const ip = await $axios.$get('https://jsonplaceholder.typicode.com/todos/1')
+          return { ip }
+        },
+      
+      methods: {
+       
+      }
+    }
+  </script>
+      
+      <template>
+        <h1>newdeal  {{ip}}</h1>
+      </template>
+          
